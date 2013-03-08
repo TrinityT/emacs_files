5bbcb85b8796e0cac4b0d87a332ed91cae46c3e1
TrinityT <trinityt.ttt@gmail.com>
2013-03-08 16:38:52 +0900

    .emacsファイルを止めてinit.elに集約

--- init.el
+++ init.el
@@ -1,8 +1,7 @@
 ;;; NOTICE: 新規環境構築する場合のコマンド例
 ;;; $ rm -rf ~/.emacs
-;;; $ rm -rf ~/.emacs.d/elisp
-;;; $ ln -fns ~/Dropbox/development/emacs_files/.emacs ~/
-;;; $ ln -fns ~/Dropbox/development/emacs_files/elisp ~/.emacs.d
+;;; $ rm -rf ~/.emacs.d
+;;; $ ln -fns ~/Dropbox/development/emacs_files ~/.emacs.d
 ;;; $ sudo apt-get install elscreen emacs-goodies-el xsel php-elisp lv
 
 ;;; デフォルトload-path
@@ -18,16 +17,16 @@
 
 ;;; auto-install設定
 (require 'auto-install)
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup)
+;  (setq auto-install-directory "~/.emacs.d/elisp/")
+;  (auto-install-update-emacswiki-package-name t)
+;  (auto-install-compatibility-setup)
 
 ;;; grep設定
 (setq grep-host-defaults-alist nil)
-(setq grep-find-template "find . <X> -type f <F> -print0 | xargs -0 -e grep -nH -e <R>") 
+(setq grep-find-template "find . <X> -type f <F> -print0 | xargs -0 -e grep -nH -e <R>")
 ;;; ※grepにlgrepを使用する場合
 ;(setq grep-template "lgrep <C> -n <R> <F> <N>")
-;(setq grep-find-template "find . <X> -type f <F> -print0 | xargs -0 -e lgrep -n -Ou8 <R>") 
+;(setq grep-find-template "find . <X> -type f <F> -print0 | xargs -0 -e lgrep -n -Ou8 <R>")
 
 ;;; デフォルト文字コード設定
 ;(set-default-coding-systems 'sjis)
@@ -247,7 +246,7 @@
                                      :background "unspecified"
                                      :strike-through nil
                                      :underline t))))))))
- 
+
 (put 'upcase-region 'disabled nil)
 
 ;; Yasnippet
@@ -271,7 +270,7 @@
 ;; f:サイズ拡大
 ;; b:サイズ縮小
 ;; C-a:画面左端へ移動
-;; q:終了．適当な位置，サイズになったら，q 
+;; q:終了．適当な位置，サイズになったら，q
 (defun my-window-ctrl ()
    "Window size and position control."
    (interactive)
@@ -339,9 +338,9 @@
 (require 'ruby-mode)
 (defun ruby-mode-set-encoding () ())
 (setq auto-mode-alist (append '(("\\.rb$" . ruby-mode)
-                                ("[Rr]akefile" . ruby-mode) 
-                                ("\\.rake$" . ruby-mode))  
-                              auto-mode-alist)) 
+                                ("[Rr]akefile" . ruby-mode)
+                                ("\\.rake$" . ruby-mode))
+                              auto-mode-alist))
 ;; Rinari
 (add-to-list 'load-path "~/.emacs.d/elisp/rinari")
 (require 'rinari)
@@ -437,93 +436,94 @@
 
 ;;# JavaScript #######################################################################
 ;;; js2-mode
+(add-to-list 'load-path "~/.emacs.d/elisp/js2-mode")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
 
 ; fixing indentation
 ; refer to http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
-(autoload 'espresso-mode "espresso")
+;; (autoload 'espresso-mode "espresso")
 
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (espresso--proper-indentation parse-status))
-           node)
+;; (defun my-js2-indent-function ()
+;;   (interactive)
+;;   (save-restriction
+;;     (widen)
+;;     (let* ((inhibit-point-motion-hooks t)
+;;            (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+;;            (offset (- (current-column) (current-indentation)))
+;;            (indentation (espresso--proper-indentation parse-status))
+;;            node)
 
-      (save-excursion
+;;       (save-excursion
 
-        ;; I like to indent case and labels to half of the tab width
-        (back-to-indentation)
-        (if (looking-at "case\\s-")
-            (setq indentation (+ indentation (/ espresso-indent-level 2))))
+;;         ;; I like to indent case and labels to half of the tab width
+;;         (back-to-indentation)
+;;         (if (looking-at "case\\s-")
+;;             (setq indentation (+ indentation (/ espresso-indent-level 2))))
 
-        ;; consecutive declarations in a var statement are nice if
-        ;; properly aligned, i.e:
-        ;;
-        ;; var foo = "bar",
-        ;;     bar = "foo";
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (setq indentation (+ 4 indentation))))
+;;         ;; consecutive declarations in a var statement are nice if
+;;         ;; properly aligned, i.e:
+;;         ;;
+;;         ;; var foo = "bar",
+;;         ;;     bar = "foo";
+;;         (setq node (js2-node-at-point))
+;;         (when (and node
+;;                    (= js2-NAME (js2-node-type node))
+;;                    (= js2-VAR (js2-node-type (js2-node-parent node))))
+;;           (setq indentation (+ 4 indentation))))
 
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
+;;       (indent-line-to indentation)
+;;       (when (> offset 0) (forward-char offset)))))
 
-(defun my-indent-sexp ()
-  (interactive)
-  (save-restriction
-    (save-excursion
-      (widen)
-      (let* ((inhibit-point-motion-hooks t)
-             (parse-status (syntax-ppss (point)))
-             (beg (nth 1 parse-status))
-             (end-marker (make-marker))
-             (end (progn (goto-char beg) (forward-list) (point)))
-             (ovl (make-overlay beg end)))
-        (set-marker end-marker end)
-        (overlay-put ovl 'face 'highlight)
-        (goto-char beg)
-        (while (< (point) (marker-position end-marker))
-          ;; don't reindent blank lines so we don't set the "buffer
-          ;; modified" property for nothing
-          (beginning-of-line)
-          (unless (looking-at "\\s-*$")
-            (indent-according-to-mode))
-          (forward-line))
-        (run-with-timer 0.5 nil '(lambda(ovl)
-                                   (delete-overlay ovl)) ovl)))))
+;; (defun my-indent-sexp ()
+;;   (interactive)
+;;   (save-restriction
+;;     (save-excursion
+;;       (widen)
+;;       (let* ((inhibit-point-motion-hooks t)
+;;              (parse-status (syntax-ppss (point)))
+;;              (beg (nth 1 parse-status))
+;;              (end-marker (make-marker))
+;;              (end (progn (goto-char beg) (forward-list) (point)))
+;;              (ovl (make-overlay beg end)))
+;;         (set-marker end-marker end)
+;;         (overlay-put ovl 'face 'highlight)
+;;         (goto-char beg)
+;;         (while (< (point) (marker-position end-marker))
+;;           ;; don't reindent blank lines so we don't set the "buffer
+;;           ;; modified" property for nothing
+;;           (beginning-of-line)
+;;           (unless (looking-at "\\s-*$")
+;;             (indent-according-to-mode))
+;;           (forward-line))
+;;         (run-with-timer 0.5 nil '(lambda(ovl)
+;;                                    (delete-overlay ovl)) ovl)))))
 
-(defun my-js2-mode-hook ()
-  (require 'espresso)
-  (setq espresso-indent-level 4
-        indent-tabs-mode nil
-        c-basic-offset 4)
-  (c-toggle-auto-state 0)
-  (c-toggle-hungry-state 1)
-  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-  ; (define-key js2-mode-map [(meta control |)] 'cperl-lineup)
-  (define-key js2-mode-map "\C-\M-\\"
-    '(lambda()
-       (interactive)
-       (insert "/* -----[ ")
-       (save-excursion
-         (insert " ]----- */"))
-       ))
-  (define-key js2-mode-map "\C-m" 'newline-and-indent)
-  ; (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
-  ; (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
-  (define-key js2-mode-map "\C-\M-q" 'my-indent-sexp)
-  (if (featurep 'js2-highlight-vars)
-      (js2-highlight-vars-mode))
-  (message "My JS2 hook"))
+;; (defun my-js2-mode-hook ()
+;;   (require 'espresso)
+;;   (setq espresso-indent-level 4
+;;         indent-tabs-mode nil
+;;         c-basic-offset 4)
+;;   (c-toggle-auto-state 0)
+;;   (c-toggle-hungry-state 1)
+;;   (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+;;   ; (define-key js2-mode-map [(meta control |)] 'cperl-lineup)
+;;   (define-key js2-mode-map "\C-\M-\\"
+;;     '(lambda()
+;;        (interactive)
+;;        (insert "/* -----[ ")
+;;        (save-excursion
+;;          (insert " ]----- */"))
+;;        ))
+;;   (define-key js2-mode-map "\C-m" 'newline-and-indent)
+;;   ; (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
+;;   ; (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
+;;   (define-key js2-mode-map "\C-\M-q" 'my-indent-sexp)
+;;   (if (featurep 'js2-highlight-vars)
+;;       (js2-highlight-vars-mode))
+;;   (message "My JS2 hook"))
 
-(add-hook 'js2-mode-hook 'my-js2-mode-hook)
+;; (add-hook 'js2-mode-hook 'my-js2-mode-hook)
 
 ;;# anything.el #######################################################################
 (setq anything-mode-line-string nil)
